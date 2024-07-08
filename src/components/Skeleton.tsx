524e3c1997ffb219de0d4f27ef914f9a095d4e6e
Jerry Chiu <m08632000@hotmail.com>
2024-07-08 15:50:25 +0800

    add skeleton component

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,9 +1,33 @@
 import classNames from "classnames";
 import { twMerge } from "tailwind-merge";
 
-export default function Skeleton({ times }: { times: number }) {
+export default function Skeleton({ times, className }: { times: number, className: string }) {
+
+
   const boxes = Array(times).fill(0).map((_, i) => {
-    return <div key={i} />
+    const outerClasses = twMerge(classNames(
+      'relative',
+      'overflow-hidden',
+      'bg-gray-200',
+      'rounded',
+      'mb-2.5',
+      className
+    ));
+    const innerClasses = twMerge(classNames(
+      'animate-shimmer',
+      'absolute',
+      'inset-0',
+      '-translate-x-full',
+      'bg-gradient-to-r',
+      'from-gray-200',
+      'via-white',
+      'to-gray-200'
+    ))
+    return (
+      <div key={i} className={outerClasses}>
+        <div className={innerClasses} />
+      </div>
+    )
   })
 
   return boxes
--- src/store/thunks/fetchUsers.tsx
+++ src/store/thunks/fetchUsers.tsx
@@ -13,7 +13,7 @@ export const fetchUsers = createAsyncThunk<User[], void, { rejectValue: string }
   const { data, status } = await axios.get('http://localhost:3005/users') as ResponseType;
 
   // DEV ONLU
-  await pause(1000)
+  await pause(90000)
 
   if (status < 200 || status >= 300) {
     return rejectWithValue('An unknow error!')
